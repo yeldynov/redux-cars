378a6f834b3e3e09afe6833294ee3086b63cf8d2
Nico Yeldynov <yeldynov@gmail.com>
2023-04-06 16:03:59 +0300

    init commit

---
+++ src/components/CarSearch.jsx
@@ -0,0 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { changeSearchTerm } from '../store';
+
+function CarSearch() {
+  const dispatch = useDispatch();
+  const searchTerm = useSelector((state) => state.cars.searchTerm);
+
+  const handelSearchTermChange = (event) => {
+    dispatch(changeSearchTerm(event.target.value));
+  };
+
+  return (
+    <div className='list-header'>
+      <h3 className='title is-3'>My Cars</h3>
+      <div className='search field is-horizontal'>
+        <label className='label'>Search</label>
+        <input
+          type='text'
+          className='input'
+          value={searchTerm}
+          onChange={handelSearchTermChange}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default CarSearch;
